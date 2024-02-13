02e58021e4007ca1cdfd700243ce1669237cebeb
RUSHI PANCHAL <panchalrushi2182004@gmail.com>
2024-02-13 18:29:57 +0530

    updated js
    not going good 😐

--- interact.js
+++ interact.js
@@ -1,39 +1,65 @@
 const submit = document.querySelector("#btnAddTodo");
 const todoList = document.querySelector("#todoList");
+const displayTodos = document.querySelector("#todos");
 
-submit.addEventListener("click", ()=>{
-    event.preventDefault(); // Prevent default form submission behavior or prevent from refrashing automaticaly
+submit.addEventListener("click", (e)=>{
+    e.preventDefault(); // Prevent default form submission behavior or prevent from refrashing automaticaly
     const todo = document.querySelector("#inputTodo").value;  // we have to initialize todo here so it updates on evry submit
+
+    function newUl(todoss){
+        // create new ul, insite ul create new ol
+        // put content inside the ol
+    
+        let ul = document.createElement("ul");
+        let ol = document.createElement("ol");
+
+        ol.innerText = todoss;
+        ul.innerHTML = ul;
+
+        displayTodos.appendChild(ol);
+        
+        console.log("run");
+        
+    }
 
     if(todo !== ""){
 
         // todoList.innerHTML += "<li>" + todo + "</li>"; This is also an solution
         
-        const li = document.createElement("li");  // creating new element "li"
+        let ol = document.createElement("ol");  // creating new element "li"
         const btn = document.createElement("button");  // creating new element "button"
         const checkbox = document.createElement("input");
+        const br = document.createElement("br");
         checkbox.type = "checkbox";
+        // const newUL = newUl();
 
-        li.textContent = todo;
+        ol.textContent = todo;
         btn.textContent = "DELETE";
 
         todoList.appendChild(checkbox);
-        todoList.appendChild(li);  // appending the newly created li element to todoList to remove the unnecessary content
+        // todoList.appendChild(ol);  // appending the newly created li element to todoList to remove the unnecessary content
+        newUl(todo);
         todoList.appendChild(btn);
+        todoList.appendChild(br);
+
+        document.querySelector("#inputTodo").value = "";
 
         btn.addEventListener("click", ()=>{
             todoList.removeChild(checkbox);
-            todoList.removeChild(li);
+            // todoList.removeChild(ol);
+
             todoList.removeChild(btn);
+            todoList.removeChild(br);
+
         })
 
         checkbox.addEventListener("change", ()=>{
             if (checkbox.checked) {
                 // console.log("checked");
-                li.classList.add("done");
+                ol.classList.add("done");
             } else {
                 // console.log("not checked");
-                li.classList.remove("done");
+                ol.classList.remove("done");
 
             }
         })
